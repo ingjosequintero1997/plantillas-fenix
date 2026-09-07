915ac2ba4d2b3a4cada6d6e55d0e0173535b8954
Tu Nombre <tu-email@ejemplo.com>
2026-09-07 10:23:01 -0500

    chore: clean up temp debug scripts

--- backend/_check_idx.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.insert(0, '.')
-from gestante_config import build_gestante_template
-
-template = build_gestante_template()
-
-for i in range(80, 100):
-    if i < len(template):
-        print(f'{i}: {template[i]["name"]!r}')
--- backend/_show_none.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.insert(0, '.')
-from template_to_db_map import TEMPLATE_TO_DB_EXPLICIT
-from gestante_config import build_gestante_template
-
-template = build_gestante_template()
-for i in range(len(template)):
-    if TEMPLATE_TO_DB_EXPLICIT.get(i) is None:
-        print(f'{i}: {template[i]["name"]}')
